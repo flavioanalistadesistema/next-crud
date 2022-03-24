b20def0efcb70371006e845adf30a8ff729d4a5b
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-03-24 15:42:25 -0300

    First commit - project next-crud

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex justify-center h-screen items-center
+      bg-gradient-to-r from-green-500 to-blue-500
+      
+    `}>
+      <h1 className="text-6xl font-bold underline">
+        Texto
+      </h1>
+    </div>
+  )
+}
