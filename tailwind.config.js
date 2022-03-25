ae6bc4e61e4037118c0f8db4a5c5ad4fe6ed15aa
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-03-25 16:57:16 -0300

    feat: configutration in aplicattion tailwind class css
    configuration of our tailwind to avoid errors in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {  
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}"
+    ],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+  },
   theme: {
     extend: {},
   },
